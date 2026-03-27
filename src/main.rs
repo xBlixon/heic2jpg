580cd5c42604ff1507d02467cc06bf5496f33b61
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-03-27 22:09:12 +0100

    Changed final directory from today date to the name of the source folder

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,24 @@ use chrono::{Local};
 
 fn convert_all(source: &String, destination: &String) -> () {
     let source_dir = absolute(source).unwrap();
-    let destination_dir = absolute(destination).unwrap().join(today_directory());
-    match create_dir_all(destination_dir.to_str().unwrap()) {
-        Ok(_) => (),
-        Err(_) => {
-            println!("Today's directory already exists. Proceeding...");
-        },
-    }
+    // let destination_dir = absolute(destination).unwrap().join(today_directory());
+    let destination_dir = absolute(destination).unwrap();
 
     read_dir(source_dir).unwrap().for_each(|dir_file_result| {
         let dir_file = dir_file_result.unwrap();
         if !dir_file.file_type().unwrap().is_dir() {
             return;
+        }
+
+        let dir_name = dir_file.file_name().to_str().unwrap().to_owned();
+        let final_dir = destination_dir.join(dir_name);
+
+
+        match create_dir_all(final_dir.to_str().unwrap()) {
+            Ok(_) => (),
+            Err(_) => {
+                println!("Today's directory already exists. Proceeding...");
+            },
         }
 
         read_dir(dir_file.path()).unwrap().for_each(|image_file_result| {
@@ -30,7 +36,7 @@ fn convert_all(source: &String, destination: &String) -> () {
             let jpeg = convert_to_jpeg(&image_path);
             let mut jpeg_filename = image_file.path().file_stem().unwrap().display().to_string();
             jpeg_filename.push_str(".jpg");
-            jpeg.save_with_format(destination_dir.join(jpeg_filename), ImageFormat::Jpeg).unwrap();
+            jpeg.save_with_format(final_dir.join(jpeg_filename), ImageFormat::Jpeg).unwrap();
             println!("CONVERTED!");
         });
     })
@@ -130,7 +136,7 @@ fn main() -> Result<()> {
 
     convert_all(&source_dir, &destination_dir);
 
-    wipe_source_dir(&source_dir);
+    // wipe_source_dir(&source_dir);
 
     Ok(())
 }
